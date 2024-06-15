e2c69c14c3b02eaa77253d9463d0d1594caae6f1
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2024-06-15 06:25:40 -0700

    Remove deprecated pint registry syntax (#16)
    * Remove deprecated pint registry
    
    * Test suite
    
    * jaxlib

--- src/jpu/registry.py
+++ src/jpu/registry.py
@@ -1,66 +1,13 @@
-from typing import Any, Generic
-
+import pint
 from jax.tree_util import register_pytree_node
-from pint import facets
-from pint.compat import TypeAlias  # type: ignore
-from pint.facets.plain import GenericPlainRegistry, PlainUnit, QuantityT, UnitT
+from pint.compat import TypeAlias
 
 from jpu.quantity import JpuQuantity
 
 
-class GenericJpuRegistry(
-    Generic[QuantityT, UnitT], GenericPlainRegistry[QuantityT, UnitT]
-):
-    pass
-
-
-class JpuRegistry(GenericPlainRegistry[JpuQuantity[Any], PlainUnit]):
-    Quantity: TypeAlias = JpuQuantity[Any]  # type: ignore
-    Unit: TypeAlias = PlainUnit
-
-
-class GenericUnitRegistry(  # type: ignore
-    Generic[facets.QuantityT, facets.UnitT],
-    facets.GenericSystemRegistry[facets.QuantityT, facets.UnitT],
-    facets.GenericContextRegistry[facets.QuantityT, facets.UnitT],
-    GenericJpuRegistry[facets.QuantityT, facets.UnitT],
-    facets.GenericNumpyRegistry[facets.QuantityT, facets.UnitT],
-    facets.GenericMeasurementRegistry[facets.QuantityT, facets.UnitT],
-    facets.GenericFormattingRegistry[facets.QuantityT, facets.UnitT],
-    facets.GenericNonMultiplicativeRegistry[facets.QuantityT, facets.UnitT],
-    facets.GenericPlainRegistry[facets.QuantityT, facets.UnitT],
-):
-    pass
-
-
-class Quantity(
-    facets.SystemRegistry.Quantity,
-    facets.ContextRegistry.Quantity,
-    JpuRegistry.Quantity,
-    facets.NumpyRegistry.Quantity,
-    facets.MeasurementRegistry.Quantity,
-    facets.FormattingRegistry.Quantity,
-    facets.NonMultiplicativeRegistry.Quantity,
-    facets.PlainRegistry.Quantity,
-):
-    pass
-
-
-class Unit(
-    facets.SystemRegistry.Unit,
-    facets.ContextRegistry.Unit,
-    facets.NumpyRegistry.Unit,
-    facets.MeasurementRegistry.Unit,
-    facets.FormattingRegistry.Unit,
-    facets.NonMultiplicativeRegistry.Unit,
-    facets.PlainRegistry.Unit,
-):
-    pass
-
-
-class UnitRegistry(GenericUnitRegistry[Quantity, Unit]):
-    Quantity: TypeAlias = Quantity  # type: ignore
-    Unit: TypeAlias = Unit
+class UnitRegistry(pint.registry.GenericUnitRegistry[JpuQuantity, pint.Unit]):
+    Quantity: TypeAlias = JpuQuantity
+    Unit: TypeAlias = pint.Unit
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
